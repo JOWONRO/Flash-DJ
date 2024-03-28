e427f2ff9e89bd21fb55b9936b137de8cb19512b
klleon-web-wonrojo <wonro.jo@klleon.io>
2024-03-28 12:08:59 -0600

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.config.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'import/no-unresolved': ['error', { ignore: ['.svg'] }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '[@]common/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+}
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react-swc'
+import autoprefixer from 'autoprefixer'
+import cssnanoPlugin from 'cssnano'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer(), cssnanoPlugin()],
+    },
+  },
+})
