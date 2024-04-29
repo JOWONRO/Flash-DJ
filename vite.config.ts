7ba1344e4739617ba50006ef64824689bba44153
klleon-web-wonrojo <wonro.jo@klleon.io>
2024-04-29 11:34:19 -0600

    fix: postcss plugins 타입 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [autoprefixer(), cssnanoPlugin()],
+      plugins: [autoprefixer() as never, cssnanoPlugin() as never],
     },
   },
   resolve: {
