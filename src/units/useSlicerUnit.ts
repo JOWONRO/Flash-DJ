d438568f630d8914a77042b2f3eb41a9a814334f
JOWONRO <dnjsfh611@naver.com>
2024-06-02 00:02:11 -0600

    feat: update slicer unit

--- src/units/useSlicerUnit.ts
+++ src/units/useSlicerUnit.ts
@@ -1,35 +1,54 @@
 import useUnitHandler from '@src/hooks/useUnitHandler'
-import useSlicerNode from '@src/nodes/useSlicerNode'
-import { slicerOptions } from '@src/processors/options'
+import useGainNode from '@src/nodes/useGainNode'
+import useOscillatorNode from '@src/nodes/useOscillatorNode'
 import { UnitType } from '@src/types'
 
-const { frequency, depth } = slicerOptions
-
 const useSlicerUnit: UnitType = (id = 'slicer-unit') => {
-  const { audioNode, controllers, handler } = useSlicerNode({
+  const { audioNode: gainNode, handler: gainHandler } = useGainNode()
+  const {
+    audioNode: lfoNode,
+    controllers: lfoControllers,
+    handler: lfoHandler,
+  } = useOscillatorNode('square', {
     frequency: {
       id: 'Frequency',
-      defaultValue: frequency.defaultValue,
-      max: frequency.maxValue,
-      min: frequency.minValue,
+      defaultValue: 0,
+      max: 20,
+      min: 0,
       step: 0.1,
     },
-    depth: {
-      id: 'Depth',
-      defaultValue: depth.defaultValue,
-      max: depth.maxValue,
-      min: depth.minValue,
-      step: 0.01,
-    },
   })
+  const {
+    audioNode: lfoGainNode,
+    controllers: lfoGainControllers,
+    handler: lfoGainHandler,
+  } = useGainNode({
+    id: 'Depth',
+    defaultValue: 0,
+    max: 1,
+    min: 0,
+    step: 0.01,
+  })
+
+  const controllers = [lfoControllers, lfoGainControllers]
 
   const unitHandler = useUnitHandler({
-    initialize: handler.initialize,
-    connect: audioNode,
-    reset: [controllers],
+    initialize: async context => {
+      await gainHandler.initialize(context)
+      await lfoHandler.initialize(context)
+      await lfoGainHandler.initialize(context)
+    },
+    connect: prevNode => {
+      if (!gainNode || !lfoNode || !lfoGainNode) return
+      lfoNode.connect(lfoGainNode)
+      lfoGainNode.connect(gainNode.gain)
+      prevNode.connect(gainNode)
+      return gainNode
+    },
+    reset: controllers,
   })
 
-  return { id, controllers: [controllers], unitHandler }
+  return { id, controllers, unitHandler }
 }
 
 export default useSlicerUnit
