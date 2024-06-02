d94d3bc9415ce8d716ce808447c2ec30bea18d98
JOWONRO <dnjsfh611@naver.com>
2024-06-02 00:10:22 -0600

    chore: about update tremolo unit

--- src/processors/options.ts
+++ src/processors/options.ts
@@ -8,11 +8,6 @@ export const bitCrusherOptions = {
   },
 }
 
-export const tremoloOptions = {
-  frequency: { name: 'frequency', defaultValue: 0, maxValue: 20, minValue: 0 },
-  depth: { name: 'depth', defaultValue: 0, maxValue: 1, minValue: 0 },
-}
-
 export const phaserOptions = {
   rate: { name: 'rate', defaultValue: 0, minValue: 0, maxValue: 10 },
   depth: { name: 'depth', defaultValue: 0, minValue: 0, maxValue: 1 },
